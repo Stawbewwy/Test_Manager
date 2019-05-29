d4d4bf91c160272c15f2491b2b5118fe239b0d46
Stawbewwy <saothman@ucdavis.edu>
2019-05-29 00:17:30 -0700

    cleanup of comments

--- src/Program_Settings.cpp
+++ src/Program_Settings.cpp
@@ -27,20 +27,13 @@ void Program_Settings::print_settings()
     std::cout << "Input Sequence: " <<input_sequence << std::endl;
     std::cout << "Num Lines Down: " <<num_lines_down << std::endl;
 
-    //std::ofstream records;
 
     //Note that we append the / ourselves. The user must not input it.
     std::string record_location = output_directory + "/" + output_name;
     std::string input_file = record_location + ".input";
     std::string meta_file = record_location + ".meta";
 
-    // records.open( record_location, std::ios::out | std::ios::app );
-    // records << "#$#" << std::endl
-    //         << program_name << std::endl 
-    //         << input_sequence << std::endl
-    //         << num_lines_down << std::endl;
-    // records.close();
-
+    //Going to write two new info files.
     std::ofstream inputfs( input_file, std::ios::out | std::ios::app );
     std::ofstream metafs ( meta_file, std::ios::out | std::ios::app );
     
