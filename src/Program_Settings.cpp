7c84f1770e54eb51e3280af84574a4f727529e72
Stawbewwy <saothman@ucdavis.edu>
2019-05-29 00:09:10 -0700

    Refactor code. separate program input and rest of info away from each other

--- src/Program_Settings.cpp
+++ src/Program_Settings.cpp
@@ -3,6 +3,21 @@
 #include <iostream>
 #include <fstream>
 
+static std::string parse_input(std::string input)
+{
+    std::string temp = input;
+
+
+    for(int n = 0; n < input.length(); n++)
+    {
+        if(input[n] == ','){
+            temp[n] = '\n';
+        }
+    }
+
+    return temp;
+}
+
 void Program_Settings::print_settings()
 {
     std::cout << "\n\n==== WRITTING SETTINGS OBJECT ====\n";
@@ -12,16 +27,30 @@ void Program_Settings::print_settings()
     std::cout << "Input Sequence: " <<input_sequence << std::endl;
     std::cout << "Num Lines Down: " <<num_lines_down << std::endl;
 
-    std::ofstream records;
+    //std::ofstream records;
 
     //Note that we append the / ourselves. The user must not input it.
     std::string record_location = output_directory + "/" + output_name;
-    records.open( record_location, std::ios::out | std::ios::app );
-    records << "#$#" << std::endl
-            << program_name << std::endl 
-            << input_sequence << std::endl
-            << num_lines_down << std::endl;
-    records.close();
+    std::string input_file = record_location + ".input";
+    std::string meta_file = record_location + ".meta";
+
+    // records.open( record_location, std::ios::out | std::ios::app );
+    // records << "#$#" << std::endl
+    //         << program_name << std::endl 
+    //         << input_sequence << std::endl
+    //         << num_lines_down << std::endl;
+    // records.close();
+
+    std::ofstream inputfs( input_file, std::ios::out | std::ios::app );
+    std::ofstream metafs ( meta_file, std::ios::out | std::ios::app );
+    
+    std:: cout << parse_input(input_sequence) << "xd" << std::endl;
+    inputfs << parse_input(input_sequence);
+    inputfs.close();
+
+    metafs << program_name << std::endl
+           << num_lines_down;
+    metafs.close();
 
     std::cout << "OK. Program added to records!\n\n"; 
 }
