ffb066eb090655c487a302f79ea384601f163642
Stawbewwy <saothman@ucdavis.edu>
2019-05-29 00:10:22 -0700

    begin work on generate_program

--- src/generate_program.cpp
+++ src/generate_program.cpp
@@ -1,1 +1,27 @@
 #include "../include/generate_program.h"
+#include <iostream>
+#include <fstream>
+
+void create_source_code(std::string tester_name)
+{
+    std::string source;
+
+    // source = "#include <iostream>\n
+    //           \n
+    //           int main()\n
+    //           {
+    //               while
+    //           }
+    //           "
+}
+
+void generate_program()
+{
+    std::string tester_name;
+    std::cout << "Enter the name of the tester you want to generate: ";
+    getline(std::cin, tester_name);
+
+    create_source_code(tester_name);
+
+    return ;
+}
