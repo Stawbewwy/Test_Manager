cccecb7868851120318b4d14dd98a2855369ac41
Stawbewwy <saothman@ucdavis.edu>
2019-06-12 00:01:43 -0700

    Development of generate_program.cpp

--- src/generate_program.cpp
+++ src/generate_program.cpp
@@ -1,10 +1,56 @@
 #include "../include/generate_program.h"
 #include <iostream>
 #include <fstream>
+#include <fcntl.h>
+#include <unistd.h>
 
 void create_source_code(std::string tester_name)
 {
     std::string source;
+    
+    char curr_inp;
+
+    int num_lines_down;
+    char input, ans;
+    
+    /** READ THE META DETA FILE*/
+
+    std::ifstream meta_file((tester_name + ".meta"));
+    std::ifstream input_file( ( tester_name + ".input"));
+    
+    
+    // Pull the file name of the desired tester.
+    getline(meta_file,source);
+
+    //Pull the number of lines down out of meta file.
+    meta_file >> num_lines_down;
+
+    int stdin_backup = dup(STDIN_FILENO);
+
+    while ( input_file >> input >> ans  )
+    {
+        remove( (tester_name + ".test").c_str() );
+        
+        int test_fd = open((tester_name + ".test").c_str(), O_RDWR);
+
+        int curr_test = fork();
+
+        //Child
+        if(curr_test == 0)
+        {
+
+        }
+
+        //otherwise, we're parent and we process data.
+        
+    }
+
+    /** ASSIGN INPUT TO BE THE*/ 
+
+
+
+    //Pull
+
 
     // source = "#include <iostream>\n
     //           \n
