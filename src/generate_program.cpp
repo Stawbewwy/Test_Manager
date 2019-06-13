405f8e3b563538c646fcac6cbca3e414c9ccbf7b
Stawbewwy <saothman@ucdavis.edu>
2019-06-12 21:08:06 -0700

    Refactor code

--- src/generate_program.cpp
+++ src/generate_program.cpp
@@ -9,10 +9,11 @@
     @source:    fstream to be copied.
 
  */
-void copy_data_file(std::ofstream &destination, std::ifstream &source)
+void create_input_buffer(std::ofstream &destination, std::ifstream &source, std::string tester_name)
 {
     std::string temp;
 
+    //load the file buffer with the screen inputs
     while(source.good())
     {
         getline(source, temp);
@@ -20,9 +21,52 @@ void copy_data_file(std::ofstream &destination, std::ifstream &source)
         destination << temp << std::endl;
     }
 
+    //load the file buffer with the tests
+    std::ifstream tester_fstream( (tester_name + ".tests") );
+
+    while(tester_fstream >> temp)
+    {
+        destination << temp << endl;
+    }
+
+
     return;
 }
 
+/** This function actually runs the test for a desired input to check */
+/** @tester_name; the name of the tester program. */
+void run_test(std::string tester_name)
+{
+
+    //We will be doing the fork + exec combo to test each input.
+    //int test_fd = open((tester_name + ".buffer").c_str(), O_RDWR);
+
+    int curr_test = fork();
+        
+    //Child is the test. Rather than piping the results, we'll just leave a note in a file.
+    if(curr_test == 0)
+    {
+        int input_fd = open((tester_name + ".buffer").c_str(), O_RDONLY);
+        int output_fd = open( (tester_name + ".output").c_str(), O_RDONLY );
+
+        //change stdin to the correct input
+        dup2(input_fd,STDIN_FILENO);
+        close(input_fd);
+
+        //change stdout to a new file
+        dup2(output_fd, STDOUT_FILENO);
+        close(output_fd);
+        
+        
+        //execvp
+        
+    }
+
+    //otherwise, we're parent and we process data.
+    //wait for child, read the file, compare the results
+}
+
+/** @tester_name; the name of the tester program. */
 void create_source_code(std::string tester_name)
 {
     std::string source;
@@ -58,40 +102,10 @@ void create_source_code(std::string tester_name)
         std::ofstream tester_fstream((tester_name + ".buffer"));
         
         //Copy the inputs to get to the input to test
-        copy_data_file(tester_fstream, input_file);
+        create_input_buffer(file_name, tester_fstream, input_file);
 
-        //We will be doing the fork + exec combo to test each input.
-        int test_fd = open((tester_name + ".buffer").c_str(), O_RDWR);
-
-        int curr_test = fork();
-        
-        //Child is the test
-        if(curr_test == 0)
-        {
-            //change stdin to the correct input
-            //change stdout to a new file
-            //execvp
-        }
-
-        //otherwise, we're parent and we process data.
-        //wait for child, read the file, compare the results
-        
+        run_test(tester_name);
     }
-
-    /** ASSIGN INPUT TO BE THE*/ 
-
-
-
-    //Pull
-
-
-    // source = "#include <iostream>\n
-    //           \n
-    //           int main()\n
-    //           {
-    //               while
-    //           }
-    //           "
 }
 
 void generate_program()
