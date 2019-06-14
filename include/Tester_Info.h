0d0765bb7c91e92bcacac126f53f0455e845db93
Stawbewwy <saothman@ucdavis.edu>
2019-06-13 22:48:51 -0700

    Add file and tester name, fix bugs with class def

--- include/Tester_Info.h
+++ include/Tester_Info.h
@@ -5,13 +5,22 @@
 
 class Tester_Info
 {
-    private: 
+    public: 
     int num_lines_down, num_ans_lines;
-    std::string input, ans;
+    std::string program_name, tester_name, input, ans;
 
     public:
-    Tester_Info(int inp_num_lines_down, int inp_num_ans_lines, std::string inp_input, std::string inp_ans) 
+    Tester_Info(int inp_num_lines_down, int inp_num_ans_lines, std::string inp_program_name , std::string inp_tester_name ,
+                std::string inp_input, std::string inp_ans) 
             :   num_lines_down(inp_num_lines_down), num_ans_lines(num_ans_lines), input(inp_input), ans(inp_ans){};
+            
+    int get_num_lines_down(){ return num_lines_down; };
+    int get_num_ans_lines(){ return num_ans_lines; };
+    std::string get_program_name(){ return program_name; };
+    std::string get_tester_name(){ return tester_name; };
+    std::string get_input(){ return input; };
+    std::string get_ans(){ return ans; };
+    
 
 };
 
