5580271b03920b9bdec9f0b0412c0bae83f930f8
Stawbewwy <saothman@ucdavis.edu>
2019-06-14 20:18:33 -0700

    Fix bug: wrong value in constructor for num_ans_lines

--- include/Tester_Info.h
+++ include/Tester_Info.h
@@ -12,7 +12,7 @@ class Tester_Info
     public:
     Tester_Info(int inp_num_lines_down, int inp_num_ans_lines, std::string inp_program_name , std::string inp_tester_name ,
                 std::string inp_input, std::string inp_ans) 
-            :   num_lines_down(inp_num_lines_down), num_ans_lines(num_ans_lines), program_name(inp_program_name), tester_name(inp_tester_name) 
+            :   num_lines_down(inp_num_lines_down), num_ans_lines(inp_num_ans_lines), program_name(inp_program_name), tester_name(inp_tester_name) 
                 ,input(inp_input), ans(inp_ans){};
             
     int get_num_lines_down(){ return num_lines_down; };
