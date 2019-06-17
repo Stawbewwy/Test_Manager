1bd2006411cbf30082dba2c31dae25fe05bec52b
Stawbewwy <saothman@ucdavis.edu>
2019-06-17 16:50:52 -0700

    Keep track of the test # to have prettier output

--- include/Tester_Info.h
+++ include/Tester_Info.h
@@ -6,17 +6,18 @@
 class Tester_Info
 {
     public: 
-    int num_lines_down, num_ans_lines;
+    int num_lines_down, num_ans_lines, test_num;
     std::string program_name, tester_name, input, ans;
 
     public:
-    Tester_Info(int inp_num_lines_down, int inp_num_ans_lines, std::string inp_program_name , std::string inp_tester_name ,
+    Tester_Info(int inp_num_lines_down, int inp_num_ans_lines, int inp_test_num, std::string inp_program_name , std::string inp_tester_name ,
                 std::string inp_input, std::string inp_ans) 
-            :   num_lines_down(inp_num_lines_down), num_ans_lines(inp_num_ans_lines), program_name(inp_program_name), tester_name(inp_tester_name) 
+            :   num_lines_down(inp_num_lines_down), num_ans_lines(inp_num_ans_lines), test_num(inp_test_num) ,program_name(inp_program_name), tester_name(inp_tester_name) 
                 ,input(inp_input), ans(inp_ans){};
             
     int get_num_lines_down(){ return num_lines_down; };
     int get_num_ans_lines(){ return num_ans_lines; };
+    int get_test_num(){ return test_num; };
     std::string get_program_name(){ return program_name; };
     std::string get_tester_name(){ return tester_name; };
     std::string get_input(){ return input; };
