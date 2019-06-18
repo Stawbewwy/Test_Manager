38934fe4fdd23e4e50a27875137b5a8dab8ab3a6
Stawbewwy <saothman@ucdavis.edu>
2019-06-17 17:58:01 -0700

    add all functions to header file

--- include/run_tester.h
+++ include/run_tester.h
@@ -1,6 +1,30 @@
 #ifndef _GENERATE_PROGRAM_H
 #define _GENERATE_PROGRAM_H
-#include <stdlib.h>
+#include "../include/Tester_Info.h"
+
+std::string parse_input(std::ifstream &tests_file);
+
+std::string parse_output(std::ifstream &tests_file, int &num_ans_lines);
+
+void remove_intermediates(std::string file_name);
+
+int file_get_num_lines(std::string file_name);
+
+void create_input_buffer(std::string tester_name, std::ofstream &destination, std::ifstream &source, std::string test_input);
+
+void print_pass();
+
+void print_failed();
+
+void exec_test(std::string tester_name, std::string program_name);
+
+void run_test(Tester_Info tester_settings);
+
+void run_tests(std::string tester_name);
+
+
+
+
 
 void run_tester();
 
