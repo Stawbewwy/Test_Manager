8c60c9026730a60939072eac877f6531bdf1677d
Stawbewwy <saothman@ucdavis.edu>
2019-07-29 22:15:44 -0700

    continue adding rest of files from last commit

---
+++ include/modify_existing_tester.h
@@ -0,0 +1,6 @@
+#ifndef _UPDATE_EXISTING_PROGRAM_H
+#define _UPDATE_EXISTING_PROGRAM_H
+
+void add_remove_tests();
+
+#endif
---
+++ src/modify_existing_tester.cpp
@@ -0,0 +1,121 @@
+// Credit to Shreevardhan @Stackoverflow, https://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c 
+
+#include "../include/modify_existing_tester.h"
+#include "../include/Record.h"
+#include "../include/get_tester_input.h"
+#include <iostream>
+#include <limits>
+#include <experimental/filesystem>
+
+
+std::string get_valid_file()
+{
+    std::string temp;
+
+    std::cout << "Enter the desired file name: ";
+    getline(std::cin, temp);
+
+    return temp;
+}
+
+void edit_record_file(std::string file)
+{
+    std::string new_input;
+    std::string new_output;
+    std::string temp = "x";
+
+    std::cout << "Please enter the next input to test: ";
+    new_input = get_tester_input();
+
+    new_input += "\n";
+
+    std::cout << "Please enter the correct output for that input(enter nothing to stop): ";
+
+    //build output sequence
+    while(temp != "")
+    {
+        getline(std::cin, temp);
+        new_output += ("\n" + temp);
+    }
+    
+
+    Record new_record(new_input, new_output);
+
+    new_record.write_record(file);
+}
+void change_directory()
+{
+    std::cout << "Please enter the new desired directory: ";
+    
+    std::string temp;
+    getline(std::cin, temp);
+    
+    std::experimental::filesystem::current_path(temp);
+     return;
+}
+
+void list_curr_dir()
+{
+    std::cout << std::endl;
+    for (const auto & entry : std::experimental::filesystem::directory_iterator( std::experimental::filesystem::current_path() ) )
+        std::cout << entry.path() << std::endl;
+    
+    std::cout << std::endl;
+    return;
+}
+
+void add_remove_tests()
+{
+    int option;
+
+    while(1)
+    {   
+        std::cout << "\n==== Update Existing Program ====" << std::endl;
+        std::cout << "\nCurrent Directory: " << std::experimental::filesystem::current_path() << std::endl << std::endl
+
+                  << "1 -- Select Tester to Edit" << std::endl
+                  << "2 -- List all Files in Current Directory" << std::endl
+                  << "3 -- Change Current Directory" << std::endl
+                  << "4 -- Return to Main Menu" << std::endl
+                  << "\nPlease enter an option: ";
+        
+        std::cin >> option;
+        
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        switch(option)
+        {
+            case 1:
+            {       
+                edit_record_file( get_valid_file() );
+                break;
+            }
+
+            case 2:
+            {
+                list_curr_dir();
+                break;
+            }
+
+            case 3:
+            {
+                change_directory();
+                break;
+            }
+           
+            case 4:
+            {
+                return;
+            }
+
+            default:
+            {
+                std::cout << "\nError! Please enter a valid choice.\n";
+                break;
+            }
+        }
+
+    }
+
+}
