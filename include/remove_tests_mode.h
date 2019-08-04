24f7127b6e3ecd294028f406b3b0389a244f691f
Stawbewwy <saothman@ucdavis.edu>
2019-08-03 23:58:51 -0700

    Begin development of remove_tests_mode

--- include/remove_tests_mode.h
+++ include/remove_tests_mode.h
@@ -1,5 +1,24 @@
 #ifndef _REMOVE_TESTS_MODE_H
 #define _REMOVE_TESTS_MODE_H
-void remove_tests_mode();
+
+/**
+*   Function that opens the .tests file for a tester and dumps it into a vector, line by line (each line is an element of vector)
+*   @tester_name: the name of the tester being converted
+*/
+vector <stirng> tests_file_to_vector(std::string tester_name);
+
+/**
+*   Function that will tkae a .tests file that is in a vector form, and create a new vector that contains all tests.
+*   @file_vector: vector that contains the .tests file (in vector form)
+*/
+std::vector <string> vectorize_tests(std::vector <string> file_vector);
+
+/**
+*   Function that will allow user to remove tests from a tester
+*   @tester_name: the name of the tester being altered
+*/
+void remove_tests_mode(std::string tester_name);
+
+
 
 #endif
--- src/remove_tests_mode.cpp
+++ src/remove_tests_mode.cpp
@@ -1,6 +1,38 @@
 #include "../include/remove_tests_mode.h"
-
-void remove_tests_mode()
+#include <fstream>
+std::vector < string> tests_file_to_vector(std::string tester_name)
 {
+    ifstream test_file( tester_name + ".TM" + "/" + tester_name + ".tests" );
+
+    std::string curr_line;
+    std::vector tests_vector;
+
+    while( test_file.good() )
+    {
+        getline(test_file, curr_line);
+        tests_vector.push(curr_line);
+    }
+
+    return tests_vector;
+}
+
+void get_tests(std::vector <string> tests_vector)
+{
+
+    if(tests_vector.size() != 0)
+    {
+        while
+    }
+}
+
+std::vector <string> vectorize_tests(std::vector <string> file_vector)
+{
+    
+}
+
+void remove_tests_mode(std::string tester_name)
+{
+    std::vector <string> tests_vector = vectorize_tests( tests_file_to_vector( tester_name ) );
+    
     return ;
 }
