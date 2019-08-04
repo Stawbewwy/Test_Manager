541176c2a2ea030ae8214f28a412b43e004d7569
Stawbewwy <saothman@ucdavis.edu>
2019-08-04 13:29:30 -0700

    Continue development of remove_tests_mode. Currently have ability to load a vector with tests and print the tests out, confirming the data is being loaded from file to test vector correctly.

--- include/remove_tests_mode.h
+++ include/remove_tests_mode.h
@@ -17,11 +17,21 @@ std::vector <std::string> tests_file_to_vector(std::string tester_name);
 std::vector <std::vector <std::string>> vectorize_tests(std::vector <std::string> file_vector);
 
 /**
+*   Function that runs through a vector with tests and prints out what each test is.
+*/
+void print_tests(std::vector <std::vector<std::string> > tests_vector );
+
+/**
+*   Function that takes all modifications to tests (removed ones) and updates the .tests file to reflect the changes
+*   @tester_name: the name of the tester being altered
+*   @tests_vector: the vector that contains the remaining tests
+*/
+void write_tests(std::string tester_name, std::vector <std::vector<std::string> > tests_vector);
+
+/**
 *   Function that will allow user to remove tests from a tester
 *   @tester_name: the name of the tester being altered
 */
 void remove_tests_mode(std::string tester_name);
-
-
 
 #endif
--- src/remove_tests_mode.cpp
+++ src/remove_tests_mode.cpp
@@ -24,25 +24,72 @@ std::vector <std::vector <std::string> > vectorize_tests(std::vector <std::strin
     int line_num = 0;
     int num_lines = file_vector.size();
 
-    while(line_num < num_lines)
+    while(line_num < num_lines - 1)
     {
         std::vector <std::string> curr_test_vector;
-        
+
         //every test inp/output ends with ~x~
         while(file_vector[line_num] != "~x~")
         {
+          //  std::cerr << "ConcernFrogeasd\n";
             curr_test_vector.push_back( file_vector[line_num++] );
         }
-
+        curr_test_vector.push_back( file_vector[line_num++] );
         tests_vector.push_back(curr_test_vector);
     }
 
     return tests_vector;
 }
 
+void print_tests(std::vector <std::vector<std::string> > tests_vector )
+{
+    for(int n = 0; n < tests_vector.size(); n++)
+    {
+        std::cout << "\nTest #" << n << "\n\n";
+        std::cout << "Input: " << std::endl;
+
+        int curr_test_line = 0;
+
+        while (tests_vector[n][curr_test_line] != "~z~")
+        {
+            std::cout << tests_vector[n][curr_test_line++] << std::endl;
+        }
+
+        std::cout << "\nOutput: " << std::endl;
+
+        //push past ~z~
+        curr_test_line++;
+
+        while(tests_vector[n][curr_test_line] != "~x~")
+        {
+            std::cout<< tests_vector[n][curr_test_line++] << std::endl;
+        }
+        
+    }
+}
+
+void write_test_changes(std::string tester_name, std::vector <std::vector<std::string> > tests_vector)
+{
+
+}
+
 void remove_tests_mode(std::string tester_name)
 {
     std::vector <std::vector <std::string>> tests_vector = vectorize_tests( tests_file_to_vector( tester_name ) );
     
+    print_tests(tests_vector);
+
+    std::string user_input;
+
+    std::cout << "Select a test to remove, or nothing to cancel: ";
+    getline(std::cin, user_input);
+
+    int users_choice = stoi(user_input);
+
+    tests_vector.erase( tests_vector.begin() + users_choice);
+
+    write_test_changes(tester_name, tests_vector);
+    
     return ;
 }
+
