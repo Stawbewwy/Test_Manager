e808c97940142c579a29244531f44cab8bbefb0c
Stawbewwy <saothman@ucdavis.edu>
2019-08-04 15:41:07 -0700

    It makes sense that once the user selects a tester to modify, they keep having the option to remove or add one, so we add the while loop in add_or_remove_menu screen

--- src/add_or_remove_menu.cpp
+++ src/add_or_remove_menu.cpp
@@ -4,45 +4,47 @@
 
 void add_or_remove_menu(std::string tester_name)
 {
-
-    std::cout << "\n\n==== Add or Remove Tests ====" << std::endl;
-        
-   // std::cout << "\nCurrent Directory: " << std::experimental::filesystem::current_path() << std::endl << std::endl;
-    std::cout << "\nSelected Tester: " << tester_name << std::endl << std::endl;
-    std::cout << "1 -- Add Tests" << std::endl;
-    std::cout << "2 -- Remove Tests" << std::endl;
-    std::cout << "3 -- Return to Previous Menu" << std::endl;
-
-    std::cout << std::endl <<"Please enter an option: ";
-
-    std::string user_input;
-    getline(std::cin, user_input);
-    int user_choice = stoi(user_input);
-
-    std::cout << std::endl;
-
-    switch(user_choice)
+    while(1)
     {
-        case 1:
-        {
-            add_tests_mode(tester_name);
-            break;
-        }
+        std::cout << "\n\n==== Add or Remove Tests ====" << std::endl;
+            
+    // std::cout << "\nCurrent Directory: " << std::experimental::filesystem::current_path() << std::endl << std::endl;
+        std::cout << "\nSelected Tester: " << tester_name << std::endl << std::endl;
+        std::cout << "1 -- Add Tests" << std::endl;
+        std::cout << "2 -- Remove Tests" << std::endl;
+        std::cout << "3 -- Return to Previous Menu" << std::endl;
 
-        case 2:
-        {
-            remove_tests_mode(tester_name);
-            break;
-        }
-        
-        case 3:
-        {
-            return;
-        }
+        std::cout << std::endl <<"Please enter an option: ";
 
-        default:
+        std::string user_input;
+        getline(std::cin, user_input);
+        int user_choice = stoi(user_input);
+
+        std::cout << std::endl;
+
+        switch(user_choice)
         {
-            break;
+            case 1:
+            {
+                add_tests_mode(tester_name);
+                break;
+            }
+
+            case 2:
+            {
+                remove_tests_mode(tester_name);
+                break;
+            }
+            
+            case 3:
+            {
+                return;
+            }
+
+            default:
+            {
+                break;
+            }
         }
     }
 }
